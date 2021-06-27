4356fd327de22a8bfe9f8776e13c0f7ffd1eab91
shivamnarkar47 <shivamnarkar47@gmail.com>
2021-06-27 16:08:11 +0530

    2 files modified

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,22 @@
 import React from 'react';
-import './Header.css'
+import './Header.css';
+import FlashOnIcon from '@material-ui/icons/FlashOn';
+import HomeIcon from '@material-ui/icons/Home';
+import LiveTvIcon from '@material-ui/icons/LiveTv';
+import VideoLibraryIcon from '@material-ui/icons/VideoLibrary';
+import SearchIcon from '@material-ui/icons/Search';
+import PersonOutlineIcon from '@material-ui/icons/PersonOutline';
 
 function Header() {
   return (
     <div className="header">
-      <h1>Header</h1>
+      <div className="header__icons">
+        <HomeIcon />
+      </div>
+      <img
+        src="https://suplari.com/wp-content/uploads/2019/09/hulu-logo-white.png"
+        alt=""
+      />
     </div>
   );
 }
